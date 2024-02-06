ebcf8709eb496fc181ca3fb916c0b016f7cdc813
Giorgi Zviadauri <giorgi410zviadauri@gmail.com>
2024-02-06 23:42:29 +0400

    Change style.

--- js/script.js
+++ js/script.js
@@ -51,6 +51,11 @@ const third_btn = document.getElementById("os-btn3");
 const fourth_btn = document.getElementById("os-btn4");
 const os_image = document.getElementById("os-img");
 
+const firstBtnImg = document.getElementById("btn1-img");
+const secondBtnImg = document.getElementById("btn2-img");
+const thirdBtnImg = document.getElementById("btn3-img");
+const fourthBtnImg = document.getElementById("btn4-img");
+
 first_btn.addEventListener("click", showFirstOutput);
 second_btn.addEventListener("click", showSecondOutput);
 third_btn.addEventListener("click", showThirdOutput);
@@ -58,32 +63,75 @@ fourth_btn.addEventListener("click", showFourthOutput);
 
 function showFirstOutput() {
   first_btn.classList.add("os-active-btn");
+  firstBtnImg.src = "/icons/burger-icon-active.svg";
+
+  secondBtnImg.src = "/icons/burger-icon-disabled.svg";
+  thirdBtnImg.src = "/icons/burger-icon-disabled.svg";
+  fourthBtnImg.src = "/icons/burger-icon-disabled.svg";
+
   second_btn.classList.remove("os-active-btn");
   third_btn.classList.remove("os-active-btn");
   fourth_btn.classList.remove("os-active-btn");
+
   os_image.src = "/img/os-main-img.png";
 }
 
 function showSecondOutput() {
   second_btn.classList.add("os-active-btn");
+  secondBtnImg.src = "/icons/burger-icon-active.svg";
+
+  firstBtnImg.src = "/icons/burger-icon-disabled.svg";
+  thirdBtnImg.src = "/icons/burger-icon-disabled.svg";
+  fourthBtnImg.src = "/icons/burger-icon-disabled.svg";
+
   first_btn.classList.remove("os-active-btn");
   third_btn.classList.remove("os-active-btn");
   fourth_btn.classList.remove("os-active-btn");
+
   os_image.src = "/img/service-1.jpg";
 }
 
 function showThirdOutput() {
   third_btn.classList.add("os-active-btn");
+  thirdBtnImg.src = "/icons/burger-icon-active.svg";
+
+  firstBtnImg.src = "/icons/burger-icon-disabled.svg";
+  secondBtnImg.src = "/icons/burger-icon-disabled.svg";
+  fourthBtnImg.src = "/icons/burger-icon-disabled.svg";
+
   second_btn.classList.remove("os-active-btn");
   first_btn.classList.remove("os-active-btn");
   fourth_btn.classList.remove("os-active-btn");
+
   os_image.src = "/img/service-2.jpg";
 }
 
 function showFourthOutput() {
   fourth_btn.classList.add("os-active-btn");
+  fourthBtnImg.src = "/icons/burger-icon-active.svg";
+
+  firstBtnImg.src = "/icons/burger-icon-disabled.svg";
+  secondBtnImg.src = "/icons/burger-icon-disabled.svg";
+  thirdBtnImg.src = "/icons/burger-icon-disabled.svg";
+
   second_btn.classList.remove("os-active-btn");
   third_btn.classList.remove("os-active-btn");
   first_btn.classList.remove("os-active-btn");
+
   os_image.src = "/img/service-3.jpg";
 }
+
+// animation
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
